ee7e857dfa8113f57ed40818db7125f7d47d120f
bchenghi <bohchenghin@gmail.com>
2020-06-03 20:14:42 +0800

    Updated inventory and equipment ui so that only one is visible

---
+++ 7-Deadly-Sins/Assets/Scripts/UI/PopUpUIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpUIManager : MonoBehaviour
+{
+    GameObject inventoryUI;
+    GameObject equipmentUI;
+
+    void Start()
+    {
+        inventoryUI = GetComponent<InventoryUI>().inventoryUI;
+        equipmentUI = GetComponent<EquipmentUI>().equipmentUI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+            equipmentUI.SetActive(false);
+        }
+        if (Input.GetButtonDown("EquipmentUI"))
+        {
+            equipmentUI.SetActive(!equipmentUI.activeSelf);
+            inventoryUI.SetActive(false);
+        }
+    }
+}
