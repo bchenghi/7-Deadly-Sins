bdfcad56281fc6a0b9cc3422c0631a25e7b69d2e
bchenghi <bohchenghin@gmail.com>
2020-06-13 02:52:56 +0800

    Added dialogue box and triggers

---
+++ 7-Deadly-Sins/Assets/PickUpDialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpDialogueTrigger : MonoBehaviour
+{
+    ITrigger trigger;
+    public GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = GetComponent<ITrigger>();
+    }
+
+    public void OnTriggerExit(Collider collider) {
+        if (collider.name == item.name)
+            trigger.Trigger();
+    }
+}
