5820062310d57a4a61acf647f678db674cfa8f5c
BennyPhoe <bennyphoe1998@gmail.com>
2020-06-23 18:00:02 +0800

    opening text

---
+++ 7-Deadly-Sins/Assets/Scripts/Opening/DialogueOpening.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Data;
+using UnityEngine.UI;
+
+public class DialogueOpening : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public Animator animation;
+
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        animation.SetTrigger("Change");
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+        }
+    }
+}
