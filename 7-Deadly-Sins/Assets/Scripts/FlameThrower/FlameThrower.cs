d1b7aee6fc4d78fd8a5543399443afad3a559719
BennyPhoe <bennyphoe1998@gmail.com>
2020-07-12 21:28:19 +0800

    Flame Thrower, companion fixes

---
+++ 7-Deadly-Sins/Assets/Scripts/FlameThrower/FlameThrower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrower : MonoBehaviour
+{
+    public int damage;
+    public float timeBetweenDamage;
+    private bool hasTakenDamage;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<EnemyStats>())
+        {
+            if (!hasTakenDamage)
+            {
+                hasTakenDamage = true;
+                other.transform.GetComponent<EnemyStats>().TakeDamage(damage);
+                StartCoroutine(damageCooldown(timeBetweenDamage));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<EnemyStats>())
+        {
+            hasTakenDamage = false;
+        }
+    }
+
+    IEnumerator damageCooldown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        hasTakenDamage = false;
+    }
+}
