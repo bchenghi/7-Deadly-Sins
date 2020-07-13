37ce833242bc9a2948043d9a80b7c5e79704745f
BennyPhoe <bennyphoe1998@gmail.com>
2020-07-13 20:28:45 +0800

    HacknSlash Updates

---
+++ 7-Deadly-Sins/Assets/Scripts/HackAndSlash/LiftMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftMovement : MonoBehaviour
+{
+    public bool level1Done;
+    public bool level2Done;
+    public Transform lift;
+    public Vector3 level2Position;
+    public Vector3 level3Position;
+    private bool PlayerEntered;
+    public Animator animator;
+    private bool doorClosed;
+    public bool reachedLevel2;
+    public bool reachedLevel3;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            PlayerEntered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            PlayerEntered = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (level1Done && PlayerEntered && !reachedLevel2)
+        {
+           
+            animator.SetBool("DoorOpen", false);
+            
+            StartCoroutine(WaitForDoorClose());
+            if (doorClosed)
+            {
+                lift.position = Vector3.MoveTowards(lift.position, level2Position, 3f * Time.deltaTime);
+                float distance = Vector3.Distance(lift.position, level2Position);
+                if (distance < 0.2f)
+                {
+                    reachedLevel2 = true;
+                    doorClosed = false;
+                }
+               
+            }
+            
+        }
+        else if (level2Done && PlayerEntered && !reachedLevel3)
+        {
+            
+            animator.SetBool("DoorOpen", false);
+            
+            StartCoroutine(WaitForDoorClose());
+            if (doorClosed)
+            {
+                lift.position = Vector3.MoveTowards(lift.position, level3Position, 3f * Time.deltaTime);
+                float distance = Vector3.Distance(lift.position, level3Position);
+                if (distance < 0.2f)
+                {
+                    reachedLevel3 = true;
+                    doorClosed = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator WaitForDoorClose()
+    {
+        yield return new WaitForSeconds(3f);
+        doorClosed = true;
+
+    }
+}
--- 7-Deadly-Sins/Assets/Scripts/LiftShopManager.cs
+++ 7-Deadly-Sins/Assets/Scripts/LiftShopManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LiftShopManager : MonoBehaviour
 {
@@ -10,6 +11,12 @@ public class LiftShopManager : MonoBehaviour
     Transform player;
     public GameObject shopUI;
     public Transform liftDoor;
+    public LiftMovement movement;
+    private bool UIShownLevel2;
+    private bool UIShownLevel3;
+    public CompanionsManager companionManager;
+    public Image[] crossesForFlameThrower;
+    private bool boughtFlameThrower;
 
     private void Start()
     {
@@ -19,45 +26,69 @@ public class LiftShopManager : MonoBehaviour
 
     public void ButtonForNonRegenAndPassive()
     {
-        companion.GetComponent<CompanionStats>().RegenerativeCompanion = false;
-        companion.GetComponent<TargetDetector>().Agressive = false;
-        GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
-        Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
-        go.transform.position = newPos;
+        if (!companionManager.maxCompanionReached)
+        {
+            companion.GetComponent<CompanionStats>().RegenerativeCompanion = false;
+            companion.GetComponent<TargetDetector>().Agressive = false;
+            GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
+            Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
+            go.transform.position = newPos;
+            companionManager.AddCompanionToList(go);
+        }
 
     }
 
     public void ButtonForNonRegenAndAggressive()
     {
-        companion.GetComponent<CompanionStats>().RegenerativeCompanion = false;
-        companion.GetComponent<TargetDetector>().Agressive = true;
-        GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
-        Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
-        go.transform.position = newPos;
+        if (!companionManager.maxCompanionReached)
+        {
+            companion.GetComponent<CompanionStats>().RegenerativeCompanion = false;
+            companion.GetComponent<TargetDetector>().Agressive = true;
+            GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
+            Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
+            go.transform.position = newPos;
+            companionManager.AddCompanionToList(go);
+        }
     }
 
     public void ButtonForRegenAndPassive()
     {
-        companion.GetComponent<CompanionStats>().RegenerativeCompanion = true;
-        companion.GetComponent<TargetDetector>().Agressive = false;
-        GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
-        Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
-        go.transform.position = newPos;
+        if (!companionManager.maxCompanionReached)
+        {
+            companion.GetComponent<CompanionStats>().RegenerativeCompanion = true;
+            companion.GetComponent<TargetDetector>().Agressive = false;
+            GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
+            Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
+            go.transform.position = newPos;
+            companionManager.AddCompanionToList(go);
+        }
     }
 
     public void ButtonForRegenAndAggressive()
     {
-        companion.GetComponent<CompanionStats>().RegenerativeCompanion = true;
-        companion.GetComponent<TargetDetector>().Agressive = true;
-        GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
-        Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
-        go.transform.position = newPos;
+        if (!companionManager.maxCompanionReached)
+        {
+            companion.GetComponent<CompanionStats>().RegenerativeCompanion = true;
+            companion.GetComponent<TargetDetector>().Agressive = true;
+            GameObject go = GameObject.Instantiate(companion, transform.position, Quaternion.identity);
+            Vector3 newPos = new Vector3(player.position.x + Random.Range(-4f, 4f), player.position.y, player.position.z + 2f);
+            go.transform.position = newPos;
+            companionManager.AddCompanionToList(go);
+        }
     }
 
     public void ButtonForFlameThrower(float timePerUse)
     {
-        player.GetComponent<PlayerFlameThrower>().UseTimePerAmmo = timePerUse;
-        Inventory.instance.Add(flameThrower);
+        if (!boughtFlameThrower)
+        {
+            foreach (Image im in crossesForFlameThrower)
+            {
+                im.enabled = true;
+            }
+            player.GetComponent<PlayerFlameThrower>().UseTimePerAmmo = timePerUse;
+            Inventory.instance.Add(flameThrower);
+            boughtFlameThrower = true;
+        }
     }
 
     public void ButtonForFlameThrowerAmmo(int quantity)
@@ -67,10 +98,26 @@ public class LiftShopManager : MonoBehaviour
         Inventory.instance.Add(ammo);
     }
 
+
     public void ButtonToCloseShopUI()
     {
         shopUI.SetActive(false);
         liftDoor.GetComponent<Animator>().SetBool("DoorOpen", true);
 
+    }
+
+    private void Update()
+    {
+        if (movement.reachedLevel2 && !UIShownLevel2)
+        {
+            shopUI.SetActive(true);
+            UIShownLevel2 = true;
+        }
+
+        if (movement.reachedLevel3 && !UIShownLevel3)
+        {
+            UIShownLevel3 = true;
+            shopUI.SetActive(true);
+        }
     }
 }
