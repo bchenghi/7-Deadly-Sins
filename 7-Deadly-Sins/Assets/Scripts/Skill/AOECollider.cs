af7fa7fbbf899ebf85154befaa33d43e4ee8d42a
BennyPhoe <bennyphoe1998@gmail.com>
2020-07-19 19:44:26 +0800

    Added Stun Spnning stars, edited AOESkill script

--- 7-Deadly-Sins/Assets/Scripts/Skill/AOECollider.cs
+++ 7-Deadly-Sins/Assets/Scripts/Skill/AOECollider.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AOECollider : MonoBehaviour
 {
+    public GameObject StunCircle;
     public float damage;
     public float stunTime;
     public bool isActive;
@@ -13,7 +14,8 @@ public class AOECollider : MonoBehaviour
         if (other.GetComponent<EnemyStats>() && isActive && other != null)
         {
             other.transform.GetComponent<EnemyStats>().TakeDamage((int)damage);
-            if (other.transform.GetComponent<EnemyStats>().currentHealth != 0)
+            Debug.Log(other.transform.GetComponent<EnemyStats>().currentHealth);
+            if (other.transform.GetComponent<EnemyStats>().currentHealth > 0)
             {
                 StartCoroutine(StunCoolDown(stunTime, other));
             }
@@ -31,17 +33,23 @@ public class AOECollider : MonoBehaviour
 
     IEnumerator StunCoolDown(float time, Collider other)
     {
+       
         if (other.transform.GetComponent<EnemyController>())
         {
+            GameObject Go = GameObject.Instantiate(StunCircle, other.transform.position, Quaternion.identity);
+            Go.transform.position = new Vector3(Go.transform.position.x, Go.transform.position.y + other.transform.GetComponent<CapsuleCollider>().height + 0.2f, Go.transform.position.z);
             other.transform.GetComponent<EnemyController>().StopMovement();
             yield return new WaitForSeconds(time);
             other.transform.GetComponent<EnemyController>().StartMovement();
+            Destroy(Go);
         } else
         {
-           
+            GameObject Go = GameObject.Instantiate(StunCircle, other.transform.position, Quaternion.identity);
+            Go.transform.position = new Vector3(Go.transform.position.x, Go.transform.position.y + Go.transform.GetComponent<CapsuleCollider>().height, Go.transform.position.z);
             other.transform.GetComponent<FinalEnemyController>().StopMovement();
             yield return new WaitForSeconds(time);
             other.transform.GetComponent<FinalEnemyController>().StartMovement();
+            Destroy(Go);
             
             
         }
